38487e025453d7180ae07a62ed216488f2c0198d
MatBenfield <MatBenfield@users.noreply.github.com>
2020-11-09 17:03:24 +0000

    fix
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/content.py
+++ python/content.py
@@ -6,7 +6,7 @@ import pathlib
 import json
 
 string_output  = "| ID | Location | River or Sea | Description |"
-string_output += "\n|- - -|- - -|- - -|- - -|- - -|"
+string_output += "\n|---|---|---|---|"
 root = pathlib.Path(__file__).parent.parent.resolve()
 with open( root / "latest.json", 'r') as filehandle:
     items = json.load(filehandle)
@@ -16,7 +16,7 @@ with open( root / "latest.json", 'r') as filehandle:
         v3 = item['long']
         v4 = item['riverOrSea']
         v5 = item['description']
-        string_output += f"\n | {v1} | {v2},{v3} | {v4} | {v5} |"
+        string_output += f"\n| {v1} | {v2},{v3} | {v4} | {v5} |"
 
 
 # processing
